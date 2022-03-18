161d5f7ca2204a51611180b83f6f6b9795fd8a5e
Luke Ganter <lucaseganter18@gmail.com>
2022-03-17 22:16:19 -0600

    added crud but fixing error

--- server.js
+++ server.js
@@ -6,12 +6,55 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
-//CRUD
-let db = new sqlite3.Database('tab');
-db.run('CREATE TABLE IF NOT EXISTS tab(id TEXT,name TEXT)');
+// crud
+//Set up DB //
 
 
 
+
+
+app.post("/add", function (req, res) {
+    db.serialize(() => {
+      db.run(
+          "INSERT INTO CNAME(email, password, address, city, state, zip) VALUES(?,?,?,?,?,?)",
+          [req.body.email, req.body.password, req.body.address, req.body.city, req.body.state, req.body.zip], 
+          function (err) {
+              if (err) {
+                  return console.log(err.message);
+                }
+                console.log("New customer has been added");
+                res.send(
+                    "New customer has been added into the database with EMAIL = " +
+              req.body.email +
+              " and password = " +
+              req.body.password +
+              " and address = " +
+              req.body.city +
+              " and city " +
+              req.body.address +
+              " and state = " +
+              req.body.state +
+              " and zip = " +
+              req.body.zip
+              );
+            }
+            );
+        });
+    });
+
+    //sign in crud
+    app.post('/view', function(req,res){
+        db.serialize(()=>{
+            db.each('SELECT id , name NAME FROM tab WHERE id =?',[req.body.id],function(err,row){
+                if (err){
+                    res.send("ERROR encouterd while displaying");
+                    return console.error(err.message);
+                }
+                res.send(` ID:  ${row.ID},    Name: ${row.NAME}`);
+                console.log("ENTRY deslayed sucsesfull")
+            });
+        });
+    });
 
 
 // express app 
